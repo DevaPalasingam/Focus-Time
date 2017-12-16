cf1db45fcfa24e033eac92014fe39c1c619d5a7d
Deva <deva.palasingam@gmail.com>
2017-12-16 16:17:26 -0600

    initial todo list

---
+++ src/components/todos-list-header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+export default class TodosListHeader extends React.Component {
+	render() {
+		return (
+			<tr>
+				<th>Task</th>
+				<th>Action</th>
+			</tr>
+		);
+	}
+}
